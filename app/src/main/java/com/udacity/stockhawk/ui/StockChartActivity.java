6fb067abbd73cb8d145499ab032a33e7897781db
ashok <ashokg.geek@gmail.com>
2017-03-12 11:25:23 +0530

    add marker to chart

--- app/src/main/java/com/udacity/stockhawk/ui/StockChartActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockChartActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.stockhawk.ui;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -7,14 +8,18 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
 import com.udacity.stockhawk.sync.QuoteSyncJob;
@@ -34,7 +39,7 @@ public class StockChartActivity extends AppCompatActivity implements LoaderManag
     private HashMap<Float, Long> xAxisLabels = new HashMap<>();
 
     @BindView(R.id.chart)
-    LineChart mLineChart;
+    LineChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,18 +49,32 @@ public class StockChartActivity extends AppCompatActivity implements LoaderManag
 
         symbol = getIntent().getExtras().getString(SYMBOL_KEY);
 
-        mLineChart.getXAxis().setLabelRotationAngle(-45f);
-        mLineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        mLineChart.getXAxis().setTextColor(Color.WHITE);
-        mLineChart.getAxisLeft().setTextColor(Color.WHITE);
-        mLineChart.getAxisLeft().setValueFormatter(new IAxisValueFormatter() {
+        setupLineChart();
+
+        getSupportLoaderManager().initLoader(STOCK_HISTORY_LOADER, null, this);
+    }
+
+    private void setupLineChart() {
+        mChart.getXAxis().setLabelRotationAngle(-45f);
+        mChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        mChart.getXAxis().setTextColor(Color.WHITE);
+        mChart.getAxisLeft().setTextColor(Color.WHITE);
+        mChart.setDoubleTapToZoomEnabled(false);
+        mChart.setNoDataTextColor(Color.WHITE);
+        mChart.setNoDataText(getString(R.string.chart_not_available));
+        mChart.getDescription().setEnabled(false);
+        mChart.setExtraRightOffset(25f);
+        mChart.getLegend().setTextColor(Color.WHITE);
+        mChart.getAxisRight().setEnabled(false);
+        mChart.setMarker(new MyMarkerView(this, R.layout.marker_view));
+        mChart.getAxisLeft().setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
-                return value + "$";
+                return value + getString(R.string.currency_symbol);
             }
         });
 
-        mLineChart.getXAxis().setValueFormatter(new IAxisValueFormatter() {
+        mChart.getXAxis().setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
                 Calendar calendar = Calendar.getInstance();
@@ -70,13 +89,6 @@ public class StockChartActivity extends AppCompatActivity implements LoaderManag
                 return "";
             }
         });
-
-        mLineChart.setDoubleTapToZoomEnabled(false);
-        mLineChart.setNoDataTextColor(Color.WHITE);
-        mLineChart.setNoDataText(getString(R.string.chart_not_available));
-        mLineChart.setDescription(null);
-        mLineChart.getAxisRight().setEnabled(false);
-        getSupportLoaderManager().initLoader(STOCK_HISTORY_LOADER, null, this);
     }
 
     @Override
@@ -109,9 +121,11 @@ public class StockChartActivity extends AppCompatActivity implements LoaderManag
                     lineDataSet.setDrawValues(false);
                     lineDataSet.setColor(Color.WHITE);
                     lineDataSet.setDrawHighlightIndicators(true);
-                    lineDataSet.setLineWidth(3f);
+                    lineDataSet.setLineWidth(2f);
+                    lineDataSet.setDrawHighlightIndicators(false);
+                    lineDataSet.setFillColor(Color.RED);
                     LineData lineData = new LineData(lineDataSet);
-                    mLineChart.setData(lineData);
+                    mChart.setData(lineData);
                 }
                 setActionBarTitle(data.getString(Contract.Quote.POSITION_NAME));
             }
@@ -126,5 +140,26 @@ public class StockChartActivity extends AppCompatActivity implements LoaderManag
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+    }
+
+    private class MyMarkerView extends MarkerView {
+
+        private TextView mContent;
+
+        public MyMarkerView(Context context, int layoutResource) {
+            super(context, layoutResource);
+            mContent = (TextView) findViewById(R.id.content);
+        }
+
+        @Override
+        public void refreshContent(Entry e, Highlight highlight) {
+            super.refreshContent(e, highlight);
+            mContent.setText(String.valueOf(e.getY()));
+        }
+
+        @Override
+        public MPPointF getOffset() {
+            return new MPPointF(-(getWidth() / 2), -getHeight());
+        }
     }
 }
